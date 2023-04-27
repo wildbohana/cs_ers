378c435dde7771eb456cf2168d3269ee8d6dbacb
wildbohana <83251325+wildbohana@users.noreply.github.com>
2023-04-27 13:10:44 +0200

    Osnova projekta

---
+++ Proxy/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.ServiceModel;
+using Common;
+
+namespace Client
+{
+    public class Program
+    {
+        //[ExcludeFromCodeCoverage]
+        static void Main(string[] args)
+        {
+            //Writer w = new Writer();
+            try
+            {
+                // TODO: dopuni App.config
+                // Otvaranje kanala
+                ChannelFactory<IProxyService> cf = new ChannelFactory<IProxyService>(new NetTcpBinding(), new EndpointAddress("net.tcp://localhost:4002/IProxyService"));
+                IProxyService kanal = cf.CreateChannel();
+            }
+            catch
+            {
+                throw new AddressAccessDeniedException("Neuspešna konekcija na server.");
+            }
+                
+            // pocetak slanja w.Start(...)
+            //w.Start(kanal);
+
+            Console.WriteLine("Klijent je završio sa radom. Pritisni bilo koji taster za izlaz...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Proxy/Common/IKlijent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Moq, NUnit, NUnit3TestAdapter
+
+namespace Common
+{
+    public interface IKlijent
+    {
+
+    }
+}
